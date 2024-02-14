1baa8aec111cd717321610720807660a83be259b
roboteck <robo.tech@hotmail.com>
2024-02-14 21:32:39 +1100

    Add files via upload

--- gpgl_random.py
+++ gpgl_random.py
@@ -72,6 +72,77 @@
 #         pass
 ####3# arr = bytearray(string, 'ascii')
 
+
+
+# # -*- coding: utf-8 -*-
+# """
+# Created on Jul 25, 2015
+
+# @author: jrm
+# """
+# from atom.api import Instance, Float, Bool, Int
+# from inkcut.device.plugin import DeviceProtocol, Model
+# from inkcut.core.utils import log
+
+
+# class HPGLConfig(Model):
+#     #: Pad option
+#     pad = Bool().tag(config=True)
+
+
+# class HPGLProtocol(DeviceProtocol):
+#     scale = Float(1021/90.0)
+
+#     #: Pad option
+#     config = Instance(HPGLConfig, ()).tag(config=True)
+
+#     def write(self, data):
+#         if self.config.pad:
+#             data += "\n"
+#         super().write(data)
+
+#     def connection_made(self):
+#         #: Initialize in absoulte mode
+#         self.write("IN;")
+
+#     def move(self, x, y, z, absolute=True):
+#         """ Move the given position. If absolute is true use a PR
+#         otherwise use PA. Most of the chinese machines don't handle
+#         negative values so absolute moves only works.
+        
+#         """
+#         x, y = int(x*self.scale), int(y*self.scale)
+#         if absolute:
+#             self.write("%s%i,%i;" % ('PD' if z else 'PU', x, y))
+#         else:
+#             self.write('PR%i,%i;' % (x, y))
+
+#     def set_force(self, f):
+#         self.write("FS%i; " % f)
+        
+#     def set_velocity(self, v):
+#         self.write("VS%i;" % v)
+        
+#     def set_pen(self, p):
+#         self.write("SP%i;" % p)
+        
+#     def finish(self):
+#         # Reinitialize
+#         self.write("IN;")
+
+
+
+    # http://www.ohthehugemanatee.net/2011/07/gpgl-reference-courtesy-of-graphtec/
+    # https://github.com/tonnerre/Inkcut
+    # https://github.com/pmonta/gerber2graphtec/blob/master/graphtec.py#L19-L110
+    # https://github.com/vishnubob/silhouette/blob/master/src/silhouette.py
+    # https://github.com/vishnubob/silhouette/blob/master/src/gpgl.py
+    # https://github.com/jnweiger/robocut/blob/master/Plotter.cpp#L344-L586
+    # https://github.com/fablabnbg/inkscape-silhouette/blob/master/silhouette/Graphtec.py#L305-L419
+    # https://github.com/Skrupellos/silhouette/blob/master/decode
+    # https://github.com/Snow4DV/graphtec-gp-gl-manual/raw/main/Manual.pdf
+
+
 import time
 from warnings import warn
 
